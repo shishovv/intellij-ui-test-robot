c396d1df027a8acbe8fad5a72c743f45863114d0
Vladislav Shishov <vladislav.shishov@jetbrains.com>
2022-01-12 16:09:07 +0300

    [debug] add logging

--- ide-launcher/src/test/kotlin/com/intellij/remoterobot/launcher/CommandLineProjectTest.kt
+++ ide-launcher/src/test/kotlin/com/intellij/remoterobot/launcher/CommandLineProjectTest.kt
@@ -5,6 +5,8 @@ import com.intellij.remoterobot.fixtures.CommonContainerFixture
 import com.intellij.remoterobot.fixtures.ComponentFixture
 import com.intellij.remoterobot.search.locators.byXpath
 import com.intellij.remoterobot.stepsProcessing.StepLogger
+import com.intellij.remoterobot.stepsProcessing.StepWorker
+import com.intellij.remoterobot.stepsProcessing.step
 import com.intellij.remoterobot.utils.Locators
 import com.intellij.remoterobot.utils.hasSingleComponent
 import com.intellij.remoterobot.utils.waitFor
@@ -19,7 +21,6 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.time.Duration
-import java.util.concurrent.TimeUnit
 import javax.imageio.ImageIO
 import javax.swing.Box
 import javax.swing.JDialog
@@ -33,21 +34,29 @@ class CommandLineProjectTest {
 
         @BeforeAll
         @JvmStatic
+        fun init() {
+            StepWorker.registerProcessor(StepLogger())
+        }
+
+        @BeforeAll
+        @JvmStatic
         fun startIdea() {
             val client = OkHttpClient()
             remoteRobot = RemoteRobot("http://localhost:8082", client)
             val ideDownloader = IdeDownloader(client)
-            println("Start ide")
-            ideaProcess = IdeLauncher.launchIde(
-                ideDownloader.downloadAndExtractLatestEap(Ide.IDEA_COMMUNITY, tmpDir),
-                mapOf("robot-server.port" to 8082),
-                emptyList(),
-                listOf(ideDownloader.downloadRobotPlugin(tmpDir)),
-                tmpDir
-            )
-            println("Wait for ide started")
-            waitFor(Duration.ofSeconds(90), Duration.ofSeconds(5)) {
-                remoteRobot.isAvailable()
+            step("Start ide") {
+                ideaProcess = IdeLauncher.launchIde(
+                    ideDownloader.downloadAndExtractLatestEap(Ide.IDEA_COMMUNITY, tmpDir),
+                    mapOf("robot-server.port" to 8082),
+                    emptyList(),
+                    listOf(ideDownloader.downloadRobotPlugin(tmpDir)),
+                    tmpDir
+                )
+            }
+            step("Wait for ide started") {
+                waitFor(Duration.ofSeconds(90), Duration.ofSeconds(5)) {
+                    remoteRobot.isAvailable()
+                }
             }
         }
 
@@ -61,33 +70,47 @@ class CommandLineProjectTest {
 
     @Test
     fun commandLineProjectTest() {
-        println("Click on 'Create New Project'")
-        remoteRobot.find<CommonContainerFixture>(
-            Locators.byProperties(Locators.XpathProperty.SIMPLE_CLASS_NAME to "FlatWelcomeFrame"),
-            Duration.ofSeconds(20)
-        ).button(byXpath("""//div[contains(@defaulticon, 'createNewProject') or (@accessiblename='New Project' and @class='JBOptionButton')]"""))
-            .click()
-        println("Setup project")
-        remoteRobot.find<CommonContainerFixture>(Locators.byTypeAndProperties(JDialog::class.java, Locators.XpathProperty.ACCESSIBLE_NAME to "New Project"), Duration.ofSeconds(10)).run {
-            jList().clickItem("Java")
-            button("Next").click()
-            checkBox("Create project from template").select()
-            jList().clickItem("Command Line App")
-            button("Next").click()
-            button("Finish").click()
+        step("Click on 'Create New Project'") {
+            remoteRobot.find<CommonContainerFixture>(
+                Locators.byProperties(Locators.XpathProperty.SIMPLE_CLASS_NAME to "FlatWelcomeFrame"),
+                Duration.ofSeconds(20)
+            )
+                .button(byXpath("""//div[contains(@defaulticon, 'createNewProject') or (@accessiblename='New Project' and @class='JBOptionButton')]"""))
+                .click()
+        }
+        step("Setup project") {
+            remoteRobot.find<CommonContainerFixture>(
+                Locators.byTypeAndProperties(
+                    JDialog::class.java,
+                    Locators.XpathProperty.ACCESSIBLE_NAME to "New Project"
+                ), Duration.ofSeconds(10)
+            ).run {
+                jList().clickItem("Java")
+                button("Next").click()
+                checkBox("Create project from template").select()
+                jList().clickItem("Command Line App")
+                button("Next").click()
+                button("Finish").click()
+            }
         }
         remoteRobot.find<CommonContainerFixture>(Locators.byProperties(Locators.XpathProperty.SIMPLE_CLASS_NAME to "IdeFrameImpl"), Duration.ofSeconds(20)).run {
-            println("Close Tip of the Day")
-            find<CommonContainerFixture>(Locators.byProperties(Locators.XpathProperty.ACCESSIBLE_NAME to "Tip of the Day"), Duration.ofSeconds(20))
-                .button("Close").click()
+            step("Close Tip of the Day") {
+                find<CommonContainerFixture>(
+                    Locators.byProperties(Locators.XpathProperty.ACCESSIBLE_NAME to "Tip of the Day"),
+                    Duration.ofSeconds(20)
+                )
+                    .button("Close").click()
+            }
             runCatching { button("Got It").click() }
-            println("Run 'Main'")
-            find<CommonContainerFixture>(Locators.byType(Box::class.java))
-                .find<ComponentFixture>(Locators.byProperties(Locators.XpathProperty.TOOLTIP to "Run 'Main'"))
-                .click()
-            println("Check output")
-            waitFor(Duration.ofSeconds(30)) {
-                hasSingleComponent(Locators.byPropertiesContains(Locators.XpathProperty.TEXT to "Process finished with exit code 0"))
+            step("Run 'Main'") {
+                find<CommonContainerFixture>(Locators.byType(Box::class.java))
+                    .find<ComponentFixture>(Locators.byProperties(Locators.XpathProperty.TOOLTIP to "Run 'Main'"))
+                    .click()
+            }
+            step("Check output") {
+                waitFor(Duration.ofSeconds(30)) {
+                    hasSingleComponent(Locators.byPropertiesContains(Locators.XpathProperty.TEXT to "Process finished with exit code 0"))
+                }
             }
         }
     }
